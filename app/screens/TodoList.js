8f61250f05a3fcb151e9a88c76a5c5281579d483
hoinlee-moi <lhi3048@gmail.com>
2023-07-09 00:28:02 +0900

    test : Dummy_list add

--- app/screens/TodoList.js
+++ app/screens/TodoList.js
@@ -3,13 +3,51 @@ import {useNavigation} from '@react-navigation/native';
 import {images} from '../constants/images';
 import ImageButton from '../components/ui/ImageButton';
 
+const DUMMY_LIST = [
+  {
+    id: '0',
+    content: 'todo1',
+    date: new Date('6월 21일'),
+    checked: false,
+    tag: ['태그1'],
+  },
+  {
+    id: '1',
+    content: 'todo2',
+    date: new Date('2월 21일'),
+    checked: true,
+    tag: [],
+  },
+  {
+    id: '2',
+    content: 'todo3',
+    date: new Date('3월 29일'),
+    checked: false,
+    tag: ['태그1','태그2'],
+  },
+  {
+    id: '3',
+    content: 'todo4',
+    date: new Date('12월 21일'),
+    checked: true,
+    tag: ['태그1'],
+  },
+  {
+    id: '4',
+    content: 'todo5',
+    date: new Date('1월 5일'),
+    checked: false,
+    tag: ['태그1','태그2','태그3','태그4'],
+  },
+];
+
 const TodoList = () => {
   const navigation = useNavigation();
   const cancelHandler = () => navigation.navigate('CreateEditTodo');
 
   return (
     <View style={styles.screen}>
-      <Text>TodoList</Text>
+      <View></View>
       <ImageButton
         name={images.add}
         style={styles.buttonContainer}
@@ -24,8 +62,8 @@ const styles = StyleSheet.create({
   screen: {
     position: 'relative',
     flex: 1,
-    alignContent: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#333333',
   },
   buttonContainer: {
     position: 'absolute',
---
+++ app/util/date.js
@@ -0,0 +1,7 @@
+export const getFormattedDate = (date) => {
+    return `${date.getMonth() + 1}월${date.getDate()}일`;
+  };
+
+export const getDeadLineDate = (date,days) => {
+    return new Date()
+}
