776ffb52ec27bcd3dafa51b22084b4f5d3c0eee4
hoinlee-moi <lhi3048@gmail.com>
2023-07-08 21:56:57 +0900

    Feat : ui components imagebutton, iconbutton, imagesFile

---
+++ app/components/ui/ImageButton.js
@@ -0,0 +1,22 @@
+import {Image, TouchableOpacity, StyleSheet} from 'react-native';
+
+const ImageButton = ({name, onPress, style}) => {
+  console.log(name)
+  return (
+    <TouchableOpacity onPress={onPress} style={style}>
+      <Image style={styles.button} source={name} />
+    </TouchableOpacity>
+  );
+};
+
+export default ImageButton;
+
+const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
+  button: {
+    width: '100%',
+    height: '100%',
+  },
+});
--- app/screens/TodoList.js
+++ app/screens/TodoList.js
@@ -1,19 +1,20 @@
 import {View, Text, StyleSheet, Pressable} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {images} from '../constants/images';
+import ImageButton from '../components/ui/ImageButton';
 
 const TodoList = () => {
   const navigation = useNavigation();
-  const move = () => {
-    navigation.navigate('CreateEditTodo');
-  };
+  const cancelHandler = () => navigation.navigate('CreateEditTodo');
+
   return (
     <View style={styles.screen}>
       <Text>TodoList</Text>
-      <Pressable onPress={move}>
-        <View>
-          <Text>만들러가기</Text>
-        </View>
-      </Pressable>
+      <ImageButton
+        name={images.add}
+        style={styles.buttonContainer}
+        onPress={cancelHandler}
+      />
     </View>
   );
 };
@@ -21,8 +22,18 @@ export default TodoList;
 
 const styles = StyleSheet.create({
   screen: {
+    position: 'relative',
     flex: 1,
     alignContent: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    overflow: 'hidden',
   },
 });
