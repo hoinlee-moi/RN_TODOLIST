eef61d2afc4057796c5700ce6959fa18de443c6f
hoinlee-moi <lhi3048@gmail.com>
2023-07-08 19:53:04 +0900

    Style : code formatting

--- app/navigation/Navigation.js
+++ app/navigation/Navigation.js
@@ -1,4 +1,3 @@
-import {Button, Pressable, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
